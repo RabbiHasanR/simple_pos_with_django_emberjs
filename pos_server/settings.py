74413ebdb1b8eee6d8ad1cc5983ff04c693eb777
Rabbi50 <jasrabbi50@gmail.com>
2020-09-27 23:36:07 +0600

    connect postgresql database pos

--- pos_server/settings.py
+++ pos_server/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pos_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pos',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
