7f9bab9e41b18f16b8a2eafd366fce678016abd3
Rabbi50 <jasrabbi50@gmail.com>
2020-09-29 00:10:30 +0600

    create product update view

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import (product_create_view,product_delete_view,product_list_view,product_details_view)
+from .views import (product_create_view,product_delete_view,product_list_view,product_details_view,product_update_view)
 
 '''
 CLIENT
@@ -12,5 +12,6 @@ urlpatterns = [
     path('',product_list_view),
     path('create/',product_create_view),
     path('<int:product_id>/',product_details_view),
-    path('<int:product_id>/delete/',product_delete_view)
+    path('<int:product_id>/delete/',product_delete_view),
+    path('<int:product_id>/update/',product_update_view)
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+import json
+
 from .serializers import ProductSerializer
 from .models import Product
 
@@ -9,7 +11,7 @@ from .models import Product
 #@authentication_classes([SessionAuthentication])
 @permission_classes([IsAuthenticated]) 
 def product_create_view(request,*args,**kwargs):
-    request.data['user']=request.user.pk
+    request.data['user']=request.user.id
     serializer=ProductSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         print(request.user)
@@ -44,3 +46,17 @@ def product_delete_view(request,product_id,*args,**kwargs):
     obj=qs.first()
     obj.delete()
     return Response({'message':'Tweet removed!'},status=200)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated]) 
+def product_update_view(request,product_id,*args,**kwarg):
+    user=request.user.id
+    request.data['user']=user
+    product_object=Product.objects.filter(id=product_id,user=user)
+    if not product_object.exists():
+        return Response({},status=404)
+    serializer = ProductSerializer(product_object.first(),data=request.data) 
+    if serializer.is_valid(): 
+        serializer.save() 
+        return Response(serializer.data,status=200) 
+    return Response(serializer.errors,status=400)
