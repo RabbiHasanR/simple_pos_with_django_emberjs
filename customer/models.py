bc9eaef94df24752279e1d3381d45bc776c1571e
Rabbi50 <jasrabbi50@gmail.com>
2020-09-28 17:44:59 +0600

    d

--- customer/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Customer(models.Model):
-    """A model that contains data for a single customer."""
-    name = models.CharField(max_length=255, default="")
-    phone = models.CharField(max_length=255, default="")
-    address = models.CharField(max_length=11, default="")
--- product/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Product(models.Model):
-    """A model that contains data for a single product."""
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    title = models.CharField(max_length=255, default="")
-    available_inventory = models.PositiveIntegerField(default=0)
